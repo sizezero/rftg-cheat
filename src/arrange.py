994f102a28f6326b078f436ff3c96c1ff6010c08
Robert Kleemann <robert@kleemann.org>
2015-02-20 18:14:13 -0800

    loading images

--- src/arrange.py
+++ src/arrange.py
@@ -2,45 +2,66 @@
 # -*- coding: utf-8 -*-
 
 from gimpfu import *
+import csv
+import os.path
 
 # loads file as image
-def load(filename):
-    return pdb.gimp_file_load(filename, filename)
+def load(tile, description):
+    f = "../img/src/tiles/"+tile+".xcf"
+    if os.path.exists(f):
+        return pdb.gimp_file_load(f, f).layers[0]
+    else:
+        image = gimp.Image(1, 1, RGB)
+        # TODO make this look like a tile
+        background = gimp.Layer(image, "Background", 100, 100,
+                            RGB_IMAGE, 100, NORMAL_MODE)
+        background.fill(BACKGROUND_FILL)
+        image.add_layer(background, 0)
+        return image.layers[0]
 
-# returns a map of images
+# returns a map of tiles and layers
 def loadAll():
-    d = {}
-    for i in ['h1', 'h2', 'h3']:
-        d[i]=load(i+".xcf").layers[0]
-    return d
+    all = {}
+    with open('../docs/roll.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        # ignore two header lines
+        reader.next()
+        reader.next()
+        for row in reader:
+            id = int(row[0])
+            w = { 'cost':int(row[1]), 'name':row[2], 'layer':load("w%02d" % id, row[2]) }
+            d = { 'cost':int(row[3]), 'name':row[4], 'layer':load("d%02d" % id, row[4]) }
+            all[id] = { 'development':d, 'world':w }
+    return all
 
 def arrange():
-    d = loadAll()
+    all = loadAll()
+
+    # stick all tiles vertically in a giant image
+    
     image = gimp.Image(1, 1, RGB)
+    pad = 10
+    y = pad
+    id = 1
+    while id in all:
+        r = all[id]
 
-    background = gimp.Layer(image, "Background", 100, 100,
-                            RGB_IMAGE, 100, NORMAL_MODE)
-    background.fill(BACKGROUND_FILL)
-    image.add_layer(background, 0)
+        d = r['development']
+        layD = pdb.gimp_layer_new_from_drawable(d['layer'], image)
+        image.add_layer(layD, 0)
+        layD.set_offsets(pad, y)
 
-    mh = 0
-    for i in ['h3', 'h2', 'h1']:
-        mh = max(mh, d[i].height)
+        w = r['world']
+        layW = pdb.gimp_layer_new_from_drawable(w['layer'], image)
+        image.add_layer(layW, 0)
+        layW.set_offsets(pad+layD.width+pad, y)
 
-    pad = 5
-    x = pad
-    for i in ['h3', 'h2', 'h1']:
-        lay = pdb.gimp_layer_new_from_drawable(d[i], image)
-        image.add_layer(lay, 0)
-        lay.set_offsets(x, (mh-lay.height)/2)
-        x += lay.width + pad
+        y += pad+max(layD.height, layW.height)
+        id += 1
 
-    #image.resize(100, 100, 0, 0)
     pdb.gimp_image_resize_to_layers(image)
-
     drawable = pdb.gimp_image_get_active_layer(image)
-    imagefile = "/home/kleemann/gimp/test1.xcf"
+    imagefile = "../img/dst/test1.xcf"
     pdb.gimp_file_save(image, drawable, imagefile,  imagefile)
-
 
 arrange()
